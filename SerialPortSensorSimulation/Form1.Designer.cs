73a746a4a3ae710772d2d78640b39de2334b8480
Bona Nugroho <bona.nugroho@gmail.com>
2025-05-25 18:13:46 +0700

    Creating forms

--- SerialPortSensorSimulation/Form1.Designer.cs
+++ SerialPortSensorSimulation/Form1.Designer.cs
@@ -30,10 +30,307 @@ namespace SerialPortSensorSimulation
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.cbBaud = new System.Windows.Forms.ComboBox();
+            this.cbComPort = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.checkBoxSimultaneousData = new System.Windows.Forms.CheckBox();
+            this.btnSendAllData = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.groupBoxSensors = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.groupBox4.SuspendLayout();
+            this.groupBoxSensors.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.groupBox5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.cbBaud);
+            this.groupBox4.Controls.Add(this.cbComPort);
+            this.groupBox4.Controls.Add(this.label7);
+            this.groupBox4.Controls.Add(this.label8);
+            this.groupBox4.Location = new System.Drawing.Point(419, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(230, 82);
+            this.groupBox4.TabIndex = 6;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Com Port";
+            // 
+            // cbBaud
+            // 
+            this.cbBaud.FormattingEnabled = true;
+            this.cbBaud.Items.AddRange(new object[] { "4800", "9600", "14400" });
+            this.cbBaud.Location = new System.Drawing.Point(79, 50);
+            this.cbBaud.Name = "cbBaud";
+            this.cbBaud.Size = new System.Drawing.Size(121, 21);
+            this.cbBaud.TabIndex = 7;
+            // 
+            // cbComPort
+            // 
+            this.cbComPort.FormattingEnabled = true;
+            this.cbComPort.Location = new System.Drawing.Point(79, 27);
+            this.cbComPort.Name = "cbComPort";
+            this.cbComPort.Size = new System.Drawing.Size(121, 21);
+            this.cbComPort.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(21, 48);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 23);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "BAUD :";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(35, 25);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 23);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "COM :";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // checkBoxSimultaneousData
+            // 
+            this.checkBoxSimultaneousData.Location = new System.Drawing.Point(423, 163);
+            this.checkBoxSimultaneousData.Name = "checkBoxSimultaneousData";
+            this.checkBoxSimultaneousData.Size = new System.Drawing.Size(116, 24);
+            this.checkBoxSimultaneousData.TabIndex = 7;
+            this.checkBoxSimultaneousData.Text = "Simultaneous Data";
+            this.checkBoxSimultaneousData.UseVisualStyleBackColor = true;
+            // 
+            // btnSendAllData
+            // 
+            this.btnSendAllData.Location = new System.Drawing.Point(556, 163);
+            this.btnSendAllData.Name = "btnSendAllData";
+            this.btnSendAllData.Size = new System.Drawing.Size(93, 23);
+            this.btnSendAllData.TabIndex = 8;
+            this.btnSendAllData.Text = "Send All Data";
+            this.btnSendAllData.UseVisualStyleBackColor = true;
+            this.btnSendAllData.Click += new System.EventHandler(this.btnSendAllData_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(417, 100);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 9;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(574, 100);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 10;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // groupBoxSensors
+            // 
+            this.groupBoxSensors.Controls.Add(this.groupBox3);
+            this.groupBoxSensors.Controls.Add(this.groupBox2);
+            this.groupBoxSensors.Controls.Add(this.groupBox5);
+            this.groupBoxSensors.Location = new System.Drawing.Point(21, 17);
+            this.groupBoxSensors.Name = "groupBoxSensors";
+            this.groupBoxSensors.Size = new System.Drawing.Size(373, 333);
+            this.groupBoxSensors.TabIndex = 11;
+            this.groupBoxSensors.TabStop = false;
+            this.groupBoxSensors.Text = "Sensors";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.trackBar3);
+            this.groupBox3.Location = new System.Drawing.Point(12, 234);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(349, 82);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Humidity";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(291, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 23);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "100 mg/l";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(6, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 23);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "-10 mg/l";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(65, 25);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(220, 45);
+            this.trackBar3.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.trackBar2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 128);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(349, 82);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Fan Speed";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(291, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "100 rpm";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(21, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 23);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "0 rpm";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(65, 25);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(220, 45);
+            this.trackBar2.TabIndex = 3;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.label2);
+            this.groupBox5.Controls.Add(this.label1);
+            this.groupBox5.Controls.Add(this.trackBar1);
+            this.groupBox5.Location = new System.Drawing.Point(12, 24);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(349, 82);
+            this.groupBox5.TabIndex = 6;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Temperature";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(291, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "220° F";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(21, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "32° F";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(65, 25);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(220, 45);
+            this.trackBar1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(682, 369);
+            this.Controls.Add(this.groupBoxSensors);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnSendAllData);
+            this.Controls.Add(this.checkBoxSimultaneousData);
+            this.Controls.Add(this.groupBox4);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBoxSensors.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.GroupBox groupBox5;
+
+        private System.Windows.Forms.ComboBox cbComPort;
+        private System.Windows.Forms.ComboBox cbBaud;
+        private System.Windows.Forms.CheckBox checkBoxSimultaneousData;
+        private System.Windows.Forms.Button btnSendAllData;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnClose;
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label8;
+
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TrackBar trackBar3;
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar trackBar2;
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBoxSensors;
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
 
         #endregion
     }
--- SerialPortSensorSimulation/Form1.cs
+++ SerialPortSensorSimulation/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,84 @@ namespace SerialPortSensorSimulation
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            groupBoxSensors.Enabled = false;
+            btnClose.Enabled = false;
+            btnOpen.Enabled = true;
+            checkBoxSimultaneousData.Enabled = false;
+            btnSendAllData.Enabled = false;
+
+            string[] ports = SerialPort.GetPortNames();
+            cbComPort.Items.Clear();
+            cbComPort.Items.AddRange(ports);
+
+            cbBaud.Text = "9600";
+
+        }
+
+
+        private void btnSendAllData_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = cbComPort.Text;
+                serialPort1.BaudRate = Convert.ToInt32(cbBaud.Text);
+                serialPort1.Open();
+
+                groupBoxSensors.Enabled = true;
+                btnClose.Enabled = true;
+                btnOpen.Enabled = false;
+                checkBoxSimultaneousData.Enabled = true;
+                btnSendAllData.Enabled = true;
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                    groupBoxSensors.Enabled = false;
+                    btnClose.Enabled = false;
+                    btnOpen.Enabled = true;
+                    checkBoxSimultaneousData.Enabled = false;
+                    btnSendAllData.Enabled = false;
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+
         }
     }
 }
